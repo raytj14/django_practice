cf1c012958c8911f600ef72febe8747d673bd551
raytj14 <reitojo2001@gmail.com>
2022-04-09 18:58:37 +0900

    add idex def in app.views

--- practice1/newyear/views.py
+++ practice1/newyear/views.py
@@ -1,3 +1,10 @@
+import datetime
+
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    return render(request, "newyear/index.html", {
+        "newyear": now.manth == 1 and now.day == 1
+    })
