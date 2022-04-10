d2c2833a8534c69819f4e43236d3af2bcdc32a86
raytj14 <reitojo2001@gmail.com>
2022-04-10 21:28:00 +0900

    session task

--- practice1/tasks/views.py
+++ practice1/tasks/views.py
@@ -22,7 +22,7 @@ def add(request):
         form = NewTaskForm(request.POST)
         if form.is_valid():
             task = form.cleaned_data["task"]
-            tasks.append(task)
+            request.session["tasks"] += [task]
             return HttpResponseRedirect(reverse("tasks:index"))
         else:
             return render(request, "task/add.html", {
